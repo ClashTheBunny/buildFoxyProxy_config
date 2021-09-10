e306791b57f2447ba93d35491d55c6645f844bc6
Randall Mason <randall@mason.ch>
2021-09-10 15:10:03 -0500

    Add options and make it a real script

--- buildFoxyProxy.py
+++ buildFoxyProxy.py
@@ -1,5 +1,20 @@
 #!/usr/local/bin/python3
 
+"""Build a FoxyProxy Config programatically
+
+Usage:
+  buildFoxyProxy.py [FILE]
+  buildFoxyProxy.py (-h | --help)
+
+Arguments:
+  [FILE] YAML file describing your proxy setup
+
+Options:
+  -h, --help     Show this screen.
+
+"""
+from docopt import docopt
+
 import sys
 import json
 from ruamel.yaml import YAML
@@ -13,34 +28,41 @@ characters = string.ascii_lowercase + string.digits
 yaml=YAML()
 yaml.default_flow_style = False
 
-configConfig = yaml.load(open("FoxyProxy.yaml"))
+def buildConfig(fileName):
+	configConfig = yaml.load(open(fileName))
 
-for i, rule in enumerate(configConfig['rules']):
-    rule['blackPatterns'] = rule['denyPatterns']
-    rule['whitePatterns'] = rule['allowPatterns']
-    rule['index'] = i
-    rule['active'] = True
+	for i, rule in enumerate(configConfig['rules']):
+		rule['blackPatterns'] = rule['denyPatterns']
+		rule['whitePatterns'] = rule['allowPatterns']
+		rule['index'] = i
+		rule['active'] = True
 
-    if rule['title'] != "Default no proxy":
-        rule['blackPatterns'] += configConfig['commonDenyPatterns']
-    final_rule = configConfig['defaultRuleValues'].copy()
-    final_rule.update(rule)
-    rule = final_rule
+		if rule['title'] != "Default no proxy":
+			rule['blackPatterns'] += configConfig['commonDenyPatterns']
+		final_rule = configConfig['defaultRuleValues'].copy()
+		final_rule.update(rule)
+		rule = final_rule
 
-    for j, pattern in enumerate(rule['blackPatterns']):
-        final_pattern = configConfig['defaultPatternValues'].copy()
-        final_pattern.update(pattern)
-        rule['blackPatterns'][j] = final_pattern
-    for j, pattern in enumerate(rule['whitePatterns']):
-        final_pattern = configConfig['defaultPatternValues'].copy()
-        final_pattern.update(pattern)
-        rule['whitePatterns'][j] = final_pattern
+		for j, pattern in enumerate(rule['blackPatterns']):
+			final_pattern = configConfig['defaultPatternValues'].copy()
+			final_pattern.update(pattern)
+			rule['blackPatterns'][j] = final_pattern
+		for j, pattern in enumerate(rule['whitePatterns']):
+			final_pattern = configConfig['defaultPatternValues'].copy()
+			final_pattern.update(pattern)
+			rule['whitePatterns'][j] = final_pattern
 
-    configConfig.insert(i, ''.join(random.choice(characters) for i in range(20)), rule)
+		configConfig.insert(i, ''.join(random.choice(characters) for i in range(20)), rule)
 
-del configConfig["commonDenyPatterns"]
-del configConfig["rules"]
-del configConfig["defaultRuleValues"]
-del configConfig["defaultPatternValues"]
+	del configConfig["commonDenyPatterns"]
+	del configConfig["rules"]
+	del configConfig["defaultRuleValues"]
+	del configConfig["defaultPatternValues"]
 
-json.dump(configConfig, sys.stdout)
+	return configConfig
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Build a FoxyProxy Config Programatically')
+    output = buildConfig(arguments['FILE'])
+    json.dump(output, sys.stdout)
